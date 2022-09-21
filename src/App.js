31871cd53d5bb0bbceb72e7097098acab9c6bf8e
alperaktepee <alperaktepe98@gmail.com>
2022-09-21 16:58:07 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Axios from "axios";
+import "./App.css";
 
 function App() {
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState({
+    country: "",
+    description: "",
+    temp:273,
+    temp_min: 273,
+    temp_max: 273,
+    humidity: 0,
+  });
+ const [dataLoaded,setDataLoaded]=useState(false);
+  const searchWeather = () => {
+    Axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d4ef56f87c05c8f93dec880eb36e9d09`
+    ).then((response) => {
+      console.log(response.data);
+      setWeatherData({
+        description: response.data.weather[0].description,
+        temp: response.data.main.temp,
+        temp_min: response.data.main.temp_min,
+        temp_max: response.data.main.temp_max,
+        humidity: response.data.main.humidity,
+        country: response.data.sys.country,
+      });
+    });
+    setDataLoaded(true);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Current Weather</h1>
+      <div className="inputs">
+        <input
+          type="text"
+          onChange={(e) => {
+            setCity(e.target.value);
+          }}
+        />
+        <button onClick={searchWeather}>Search</button>
+      </div>
+      <div className="displayDataContainer">
+        {dataLoaded &&
+        <div className="data">
+          <h3>COUNTRY : {weatherData.country}</h3>
+          <h3>DESCRIPTION : {weatherData.description}</h3>
+          <h3>TEMPERATURE: {weatherData.temp-273}°C</h3>
+          <h3>MIN TEMPERATURE: {weatherData.temp_min - 273}°C</h3>
+          <h3>MAX TEMPERATURE : {weatherData.temp_max - 273}°C</h3>
+          <h3>HUMIDITY : {weatherData.humidity}</h3>
+        </div>
+}
+      </div>
     </div>
   );
 }
